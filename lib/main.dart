f631ad7b31449d51b51e5465f459ac8a4c7aa8ea
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-26 23:44:10 -0300

    renderizando de acordo com o retorno dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,27 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text("Carregando Dados...",
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                        textAlign: TextAlign.center));
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text("Carregando Dados...",
+                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                          textAlign: TextAlign.center));
+                } else {
+                  return Container(color: Colors.green);
+                }
+            }
+          }),
     );
   }
 }
